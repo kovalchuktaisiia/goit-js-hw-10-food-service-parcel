9da213f333034b5efdcb4df13ad322fdb8747bc7
kovalchuktaisiia <kovalchuk.taisiia@google.com>
2021-05-20 11:43:56 +0300

    gallery

--- src/index.js
+++ src/index.js
@@ -3,8 +3,23 @@ import menu from './menu.json';
 import cardsTamplate from './templates/cards.hbs';
 
 
-const galleryRef = document.querySelector ('.menu js-menu');
+const galleryRef = document.querySelector ('.js-menu');
 
 const cardsMarkup  = cardsTamplate (menu);
 
-galleryRef.insertAdjacentHTML('beforeend', cardsMarkup);
+galleryRef.insertAdjacentHTML( 'beforeend', cardsMarkup);
+
+const checkbox= document.querySelector ('.theme-switch__toggle');
+const body = document.querySelector('body');
+
+const Theme = {
+    LIGHT: 'light-theme',
+    DARK: 'dark-theme',
+  };
+
+  checkbox.addEventListener ('change', changeTheme);
+
+  if (!localStorage.theme) {
+      localStorage.setItem('theme', Theme.LIGHT);
+  }
+
