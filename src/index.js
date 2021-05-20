97f7ea6881c4c523c25045f630caa88d438f5613
kovalchuktaisiia <kovalchuk.taisiia@google.com>
2021-05-20 10:36:09 +0300

    new

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import './sass/main.scss';
+import menu from './menu.json';
+import cards from '../templates/cards.hbs';
+
+
+const menuRef = document.querySelector ('.menu js-menu');
+
+ menuRef.insertAdjacentHTML('beforeend', cardsMarkup);
